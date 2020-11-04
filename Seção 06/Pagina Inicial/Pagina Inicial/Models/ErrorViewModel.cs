83f4f41ec0585386a4d5c7f375987325f8e22aeb
DaviCastrofr <72817309+DaviCastrofr@users.noreply.github.com>
2020-11-04 16:19:59 -0300

    Explicação da Estrutura de um projeto

---
+++ Seção 06/Pagina Inicial/Pagina Inicial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pagina_Inicial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
